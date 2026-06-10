0582f7d3df96845e5e539b36acd9fda749921674
Wang <92787007+wangxiuchen@users.noreply.github.com>
2026-06-10 16:53:21 +0800

    Add files via upload

---
+++ gradlew.bat
@@ -0,0 +1,23 @@
+@rem Gradle startup script for Windows
+
+@if "%DEBUG%"=="" @echo off
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+set DIRNAME=%~dp0
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Classpath addition
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+@rem Execute Gradle
+"%JAVA_HOME%\bin\java.exe" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:end
+@rem End local scope for the variables with windows NT shell
+if "%ERRORLEVEL%"=="0" goto mainEnd
+:fail
+exit /b 1
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
